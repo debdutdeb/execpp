8708288319c5e0369616844cd7c81361ad23a44d
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-03-12 19:52:22 +0530

    Some more stuff

--- include/exec.cc
+++ include/exec.cc
@@ -1,11 +1,15 @@
 #include <exec.hh>
 
+#include <array>
 #include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <vector>
+
+using namespace std::string_literals;
 
 extern int errno;
 
@@ -16,7 +20,30 @@ void print_error_and_exit(int err) {
   exit(EXIT_FAILURE);
 }
 
-std::shared_ptr<CommandOutput> return_code(std::string command) {
+[[nodiscard]] char *const *get_arguments(std::string command) {
+  /**
+   * we want for each word
+   * to allocate a c string
+   * then create a c style array of strings
+   */
+  std::vector<const char *> components;
+  size_t pos = 0;
+  while ((pos = command.find(" ")) != std::string::npos) {
+    components.push_back(command.substr(0, pos).c_str());
+    command.erase(0, pos + 1);
+  }
+
+  char *args = (char *)malloc(sizeof(char *) * components.size());
+  if (args == nullptr)
+    print_error_and_exit(errno);
+
+  for (auto c : components)
+    std::cout << c << '\n';
+
+  return nullptr;
+}
+
+std::shared_ptr<CommandOutput> run_command(std::string command) {
   std::shared_ptr<CommandOutput> command_output =
       std::make_shared<CommandOutput>();
 
@@ -30,8 +57,19 @@ std::shared_ptr<CommandOutput> return_code(std::string command) {
     print_error_and_exit(errno);
 
   if (child_pid == 0) {
-    // do  stuff
+    // if (dup2(file_descriptors[1], STDOUT_FILENO) == -1)
+    //   print_error_and_exit(errno);
+    //
+    // if (dup2(file_descriptors[0], STDERR_FILENO) == -1)
+    //   print_error_and_exit(errno);
+    //
+    // close(STDOUT_FILENO);
+    // close(STDERR_FILENO);
+
+    (void)get_arguments("echo hello world"s);
+    // execvp(*argv, const_cast<char *const *>(argv));
   } else {
+
     if (wait(&command_output->return_code) != child_pid)
       print_error_and_exit(errno);
     std::cout << "return: " << command_output->return_code << std::endl;
